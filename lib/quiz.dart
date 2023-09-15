c50b74d5ff6424b398f09a68ded909b2bc075765
exalted100 <harounayomikun@gmail.com>
2023-09-15 18:59:23 +0100

    first commit

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,3 +1,56 @@
+import 'dart:developer';
 
+import 'package:flutter/material.dart';
+import 'package:quiz_app/questions_screen.dart';
 
-const Quiz extends StatefulWidget {}
+import 'package:quiz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({ super.key });
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  var activeScreen = 'start-screen';
+
+  void switchScreen() {
+    log('data: $activeScreen');
+    setState(() {
+      activeScreen = 'questions-screen';
+    });
+  }
+
+  @override
+  Widget build(context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'questions-screen') {
+      log('data: $activeScreen');
+      screenWidget = const QuestionsScreen();
+    } else {
+      log('data: $activeScreen');
+      screenWidget = StartScreen(switchScreen);
+    }
+
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 78, 13, 151),
+              Color.fromARGB(255, 107, 15, 168),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          )),
+          child: screenWidget,
+        ),
+      ),
+    );
+  }
+}
